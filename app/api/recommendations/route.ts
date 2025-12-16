8ec48af510a669ac52b2ff9b392bf163ad00fc54
Ayotowiwa <christlightsied@gmail.com>
2025-12-16 15:29:37 +0100

    Put a backup in other api if gemini fails

--- app/api/recommendations/route.ts
+++ app/api/recommendations/route.ts
@@ -1,6 +1,7 @@
 import { NextResponse } from "next/server";
 import { GoogleGenerativeAI } from "@google/generative-ai";
 
+
 export async function POST(req: Request) {
   try {
     const { category, title } = await req.json();
